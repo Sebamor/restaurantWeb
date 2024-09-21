a9ac78d029d1d8fabe8666ec2ca3a47d6c138029
seb <13894287+Sebamor@users.noreply.github.com>
2024-09-21 08:56:16 -0700

    Tried to separate css to make more modular-style code, finish contact js

--- src/about.js
+++ src/about.js
@@ -11,10 +11,24 @@ function loadAbout() {
     const contact = document.createElement('div');
     contact.setAttribute('id', 'contact');
     contactContainer.appendChild(contact);
-    // Contact email/github/linkedin
-    const social = document.createElement('p');
-    social.textContent = 'test';
-    contact.innerHTML = 'hello' + social.textContent;
+    // Contact email/github/linkedin name
+    const email = document.createElement('span');
+    email.textContent = "sebamor1995@gmail.com"
+    const github = document.createElement('span');
+    github.textContent = "https://github.com/Sebamor";
+    const linkedIn = document.createElement('span');
+    linkedIn.textContent = "https://www.linkedin.com/in/sebastian-amor-b7b7712b9";
+    // Contact whole strings
+    const contactEmail = document.createElement('p');
+    contactEmail.innerHTML = "Email: " + email.textContent;
+    contact.appendChild(contactEmail)
+    const contactGithub = document.createElement('p');
+    contactGithub.innerHTML = "Github: " + github.textContent;
+    contact.appendChild(contactGithub);
+    const contactLinkedIn = document.createElement('p');
+    contactLinkedIn.innerHTML = "LinkedIn: " + linkedIn.textContent;
+    contact.appendChild(contactLinkedIn);
+
     
     // Map Container
     const mapContainer = document.createElement('div');
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./main.css";
+import "./about.css";
 import loadLanding from "./landing.js";
 import loadAbout from "./about.js";
 
